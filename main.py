02bca55e3474109b6ee47148a5b68b88d608943c
FofLof <ethan_see@hotmail.com>
2023-01-16 03:29:16 -0800

    Basic processing code

--- main.py
+++ main.py
@@ -8,20 +8,50 @@ CameraServer.enableLogging()
 camera = CameraServer.startAutomaticCapture()
 camera.setResolution(600, 480)
 
+table = NetworkTables.getTable("/vision")
+
 
 # x = 0;
 # y = 0;
 
 def processConeInfo():
     conePipeline.process(camera)
-    # center_x_positions = []
-    # center_y_positions = []
-    # widths = []
-    # heights = []
+    center_x_positions = []
+    center_y_positions = []
+    widths = []
+    heights = []
+
+    for contour in conePipeline.find_contours_output:
+        x, y, w, h = cv2.boundingRect(contour)
+        center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
+        center_y_positions.append(y + h / 2)
+        widths.append(w)
+        heights.append(y)
+        # Publish to the '/vision' network table
+    table.putNumberArray("centerX", center_x_positions)
+    table.putNumberArray("centerY", center_y_positions)
+    table.putNumberArray("width", widths)
+    table.putNumberArray("height", heights)
 
 
 def processCubeInfo():
     cubePipeline.process(camera)
+    center_x_positions = []
+    center_y_positions = []
+    widths = []
+    heights = []
+
+    for blob in cubePipeline.find_blobs_output:
+        x, y, w, h = cv2.boundingRect(blob)
+        center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
+        center_y_positions.append(y + h / 2)
+        widths.append(w)
+        heights.append(y)
+        # Publish to the '/vision' network table
+    table.putNumberArray("cubeCenterX", center_x_positions)
+    table.putNumberArray("cubeCenterY", center_y_positions)
+    table.putNumberArray("cubeWidth", widths)
+    table.putNumberArray("cubeHeight", heights)
 
 
 def main():
